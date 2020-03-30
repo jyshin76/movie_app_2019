2e70fb938db9d21578b85e12aeb02584e3481f76
root <jyshin@trizcorp.com>
2020-03-30 02:31:13 +0000

    Updates

---
+++ precache-manifest.5d6dc338e6c6f2b182ea828be642b895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d88d4af3ea2e5edfbad34870643acb",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "c0ec0af7037e532da096",
+    "url": "/movie_app_2019/static/css/main.7807d09c.chunk.css"
+  },
+  {
+    "revision": "0039364e649503fed3b0",
+    "url": "/movie_app_2019/static/js/2.d1795647.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.d1795647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ec0af7037e532da096",
+    "url": "/movie_app_2019/static/js/main.a55d6bf3.chunk.js"
+  },
+  {
+    "revision": "cb94ca456319c7288dff",
+    "url": "/movie_app_2019/static/js/runtime-main.97eb87f9.js"
+  }
+]);
